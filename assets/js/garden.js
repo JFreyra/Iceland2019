8773a2fb2756161c202165412d39e19fd1ede712
JFreyra <julius@freyra.org>
2019-06-29 01:44:30 +0000

    set up structure for cycling dates for garden map

---
+++ assets/js/garden.js
@@ -0,0 +1,9 @@
+/*
+
+   Javascript should select slider element with class "slick-current"
+   pull possibly id which is date and select data based on pulled date
+
+   Carousel buttons have eventlisteners that trigger the javascript to
+   update stored value to new slick-current
+
+*/
