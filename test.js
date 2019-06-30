12c229ad5b2dac78224ac9a176681321dc57d151
U-DESKTOP-MI2AEV3\rebec <rebeccagartenberg1@gmail.com>
2019-06-30 13:55:46 +0000

    circle

--- test.js
+++ test.js
@@ -2,16 +2,25 @@
   document.getElementById("demo").innerHTML = "blah";
 }*/
 
+
+function createDot(x,y,r){
+  document.createElementNS("http://www.w3.org/2000/svg","circle");
+
+}
+
+//Enlarges an image
 function bigImg(x) {
 x.style.height = "256px";
 x.style.width = "256px";
 }
 
+//Sets images size to normal
 function normalImg(x) {
 x.style.height = "128px";
 x.style.width = "128px";
 }
 
+//Gets pixel positions of mouse
 $(document).ready(function () {
     $("body").mousemove(function (e) {
         var pX = e.pageX;
